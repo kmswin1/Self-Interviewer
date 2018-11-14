fedc260a376e68d8b3fef5bcbb7c5f261faf09a7
Minsang Kim <kmswin1@korea.ac.kr>
2018-11-14 15:01:56 +0900

    basic architecture in run.py

--- run.py
+++ run.py
@@ -2,8 +2,28 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def main():
     return 'Team 1'
+
+@app.route('/signin/<userid>')
+def sign_in(userid):
+    return '%s information' %userid
+
+@app.route('/question')
+def question():
+    return 'question'
+
+@app.route('/matching')
+def matching():
+    return 'matching'
+
+@app.route('/studyroom')
+def studyroom():
+    return 'studyroom'
+
+@app.route('/community')
+def community():
+    return 'community'
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0')
