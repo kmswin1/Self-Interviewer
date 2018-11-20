e062d19571135bb4a731fe94f7840a7577c1a3fb
hanjjm <40706867+hanjjm@users.noreply.github.com>
2018-11-20 15:56:38 +0900

    first commit

--- static/js/write.js
+++ static/js/write.js
@@ -1,13 +1,7 @@
-function move(url){
-//  location.href="community.html"
-}
+
+var json_data;
+
 function send(){
-  //String input = document.getElementById("stitle").value;
-  //temp = input;
-}
-function samp(){
-
-
   var nowDate = new Date();
   console.log(nowDate);
 
@@ -17,13 +11,14 @@ function samp(){
   var stime = nowDate.getMonth() + "/" + nowDate.getDate() + "/" + nowDate.getHours() + ":" + nowDate.getMinutes();
   var sview = "0";
 
-  var json_data = JSON.stringify({
+  json_data = JSON.stringify({
                 sid : sid,
                 stitle : sample,
                 stext : stext,
                 stime : stime,
                 sview : sview,
               })
+
   console.log(json_data);
 
   $.ajax({
@@ -35,19 +30,12 @@ function samp(){
             data: json_data,
             success: function (data) {
                 console.log(data);
-		console.log("success");
+
             },
             error: function (xhr) {
                 console.log (data);
-		console.log("failed");
+
             }
         });
-                console.log (data);
+        location.href="community.html"
 }
-
-
-
-//<form action="ec2-54-244-72-128.us-west-2.compute.amazonaws.com:5000/test" method="post">
-  //      <p>테스트 : <input type="text" name="test"></p>
-    //    <input type="submit" value="정보 넘기기">
-  //  </form>
