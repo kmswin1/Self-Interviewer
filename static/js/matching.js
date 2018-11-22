e5aaf969e0dabfd4496c4156fd5dfedf803d3463
Chanjoo Lee <lees3301@gmail.com>
2018-11-23 03:22:46 +0900

    add matching search button & function - /getMemeberInfo(PUT) - error : 405(Method Not Allowed) - todo : check server code

--- static/js/matching.js
+++ static/js/matching.js
@@ -1,23 +1,46 @@
-var userJsonArr = [
-    {"name":"이찬주", "location":"경기도 고양시", "text_info": "2019 취뽀!!"},
-    {"name":"이병주", "location":"경기도 광주시", "text_info": "힘을 냅시다"},
-    {"name":"이찬주", "location":"경기도 고양시", "text_info": "2019 취뽀!!"},
-    {"name":"이찬주", "location":"경기도 고양시", "text_info": "2019 취뽀!!"},
-    {"name":"이찬주", "location":"경기도 고양시", "text_info": "2019 취뽀!!"},
-    {"name":"이찬주", "location":"경기도 고양시", "text_info": "2019 취뽀!!"},
-    {"name":"이찬주", "location":"경기도 고양시", "text_info": "2019 취뽀!!"}
-]
-var myHTML = '';
-for(i = 0; i < userJsonArr.length; i++) {
-    myHTML += '<div class="card">' +
-        '<img src="../resources/img_avatar.png">' +
-        '<div class="container2">' +
-        '<h4><b>' + userJsonArr[i]['name'] + '</b></h4>' +
-        '<p>' + userJsonArr[i]['location'] +'</p>' +
-        '<p>' + userJsonArr[i]['text_info'] + '</p>' +
-        '</div>' +
-        '</div>';
-}
+var userJsonArr = ''
 
-var user_card = document.getElementById('user_card')
-user_card.innerHTML = myHTML;
+function matchingSearch() {
+    var myJSON = JSON.stringify({
+        company:'삼성전자',
+        city:'고양시',
+        town:'행신동',
+        major:'SW개발'
+    });
+
+    // 등록된 사람 불러오기
+    $.ajax({
+        type: 'PUT',
+        url: "http://ec2-54-244-72-128.us-west-2.compute.amazonaws.com:5000/getMemberInfo",
+        contentType: 'application/json; charset=utf-8',
+        traditional: true,
+        async: false,
+        data: myJSON,
+        success: function (data) {
+            //console.log(data);
+            userJsonArr = JSON.parse(data)
+            // select = userJsonArr;
+        },
+        error: function (xhr) {
+            console.log ("실패");
+            console.log(xhr)
+        }
+    });
+
+    // 카드뷰 그리기
+    var myHTML = '';
+
+    for(i = 0; i < userJsonArr.length; i++) {
+        myHTML += '<div class="card">' +
+            '<img src="../resources/img_avatar.png">' +
+            '<div class="container2">' +
+            '<h4><b>' + userJsonArr[i]['name'] + '</b></h4>' +
+            '<p>' + userJsonArr[i]['location'] +'</p>' +
+            '<p>' + userJsonArr[i]['text_info'] + '</p>' +
+            '</div>' +
+            '</div>';
+    }
+
+    var user_card = document.getElementById('user_card')
+    user_card.innerHTML = myHTML;
+}
