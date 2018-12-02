c80f84b7844f502041ecef6613922b9aef7b5f13
Minsang Kim <kmswin1@korea.ac.kr>
2018-12-02 23:06:02 +0900

    redirect url change

--- static/js/signup.js
+++ static/js/signup.js
@@ -244,7 +244,7 @@ $.ajax({
 
           }
       });
-    location.href="http://ec2-54-244-72-128.us-west-2.compute.amazonaws.com:5000"
+    location.href="http://ec2-54-244-72-128.us-west-2.compute.amazonaws.com:5000/index"
 }
 
 function collegelist(){
