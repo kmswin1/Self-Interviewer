7a64588bfdd5795981478a0dbad2ba0d8b0177a7
Minsang Kim <kmswin1@korea.ac.kr>
2018-12-02 22:19:07 +0900

    debugging

--- static/js/signup.js
+++ static/js/signup.js
@@ -131,7 +131,9 @@ function checkid(){
 
                         }
                     });
-if(check === 0) alert("중복ㅋㅋㅋ")
+                        if(check === 0) alert("아이디가 이미 존재합니다.")
+                        else alert("사용할 수 있는 아이디 입니다.")
+
 }
 
 function checknickname(){
@@ -160,7 +162,8 @@ function checknickname(){
 
                         }
                     });
-                    if(check === 0) alert("중복되었습니다");
+                    if(check === 0) alert("닉네임이 중복되었습니다");
+                    else alert("사용할 수 있는 닉네임 입니다.")
 }
 
 function random(){
