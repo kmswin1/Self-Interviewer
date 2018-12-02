553b4fa226f64b4c0199fd5deedfa9fee08659c6
Minsang Kim <kmswin1@korea.ac.kr>
2018-12-02 22:39:10 +0900

    index url fix bug

--- run.py
+++ run.py
@@ -54,8 +54,8 @@ def logIn():
         print("getUserPW success")
         if result == jsonObj["userpw"]:
             session['userid'] = jsonObj["userid"]
-            return redirect(url_for('/index'))
-    return redirect(url_for('/index'))
+            return redirect(url_for('index'))
+    return redirect(url_for('index'))
 
 
 @app.route('/logOut')
@@ -117,7 +117,7 @@ def setSignUp():
     conn.commit()
     print ("setSignUp success")
     conn.close()
-    return redirect(url_for('/index'))
+    return redirect(url_for('index'))
 
 @app.route('/sign_up')
 def sign_up():
