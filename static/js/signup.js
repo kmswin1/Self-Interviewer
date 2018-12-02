a83e446af33d7e01ff2137c0e06ce4fe5e2b113d
Minsang Kim <kmswin1@korea.ac.kr>
2018-12-02 22:29:27 +0900

    debugging in id,nickExist

--- static/js/signup.js
+++ static/js/signup.js
@@ -131,8 +131,12 @@ function checkid(){
 
                         }
                     });
-                        if(check === 0) alert("사용할 수 있는 아이디 입니다.")
-                        else if (check === 1) alert("아이디가 이미 존재합니다.")
+                        if(check === 0) {
+                            alert("사용할 수 있는 아이디 입니다.")
+                        }
+                        else if (check === 1) {
+                            alert("아이디가 이미 존재합니다.")
+                        }
 
 }
 
@@ -162,8 +166,12 @@ function checknickname(){
 
                         }
                     });
-                    if(check === 0) alert("사용할 수 있는 닉네임 입니다.");
-                    else if (check === 1) alert("닉네임이 이미 존재합니다.")
+                    if(check === 0) {
+                        alert("사용할 수 있는 닉네임 입니다.");
+                    }
+                    else if (check === 1) {
+                        alert("닉네임이 이미 존재합니다.")
+                    }
 }
 
 function random(){
